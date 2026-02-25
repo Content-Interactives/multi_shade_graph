35e97f7711e3192911dbe54794f50d3e52a6f369
User <user@example.com>
2026-02-25 15:20:55 -0800

    finished app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MultiShadeGraph from './components/MultiShadeGraph'
+
+
+function App() {
+  return (
+      <div className="App">
+        <MultiShadeGraph />
+      </div>
+  );
+}
+
+
+export default App;
